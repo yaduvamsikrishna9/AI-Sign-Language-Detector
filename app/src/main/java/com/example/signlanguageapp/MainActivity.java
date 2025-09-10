799068c9e363afe8aeedce4f4e42f57d89f30f8b
Yadu Vamsi Krishna <yaduvamsi99@gmail.com>
2025-09-10 12:32:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/signlanguageapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.signlanguageapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultText;
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultText = findViewById(R.id.resultText);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText("Detected: B");
+            }
+        });
+    }
+}
